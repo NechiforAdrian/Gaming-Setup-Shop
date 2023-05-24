ebb9e0616567d56dcc7e9a80d25158e189aa5853
NechiforAdrian <nechifor.adrian96@yahoo.com>
2023-05-24 19:04:22 +0300

    First commit

---
+++ Gaming-Setup-Shop/src/main/java/com/GamingSetupShop/GamingSetupShopApplication.java
@@ -0,0 +1,13 @@
+package com.GamingSetupShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingSetupShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingSetupShopApplication.class, args);
+	}
+
+}
---
+++ Gaming-Setup-Shop/src/test/java/com/GamingSetupShop/GamingSetupShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GamingSetupShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingSetupShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
